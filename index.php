ba6c8af92fc5987d1f4760019f67247774e5fe17
thereselyndel <dimacuhalyndel@gmail.com>
2026-08-26 13:59:46 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <div id="container">
+        <div id="header">
+            <h1>My Website</h1>
+
+        </div>
+        <div id="content">
+            <div id="nav">
+                <h3>Navigation</h3>
+                <ul>
+                    <li><a class="selected" href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+
+            </div>
+
+            <div id="main">
+                <h2>Home page</h2>
+            
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt quia eligendi, modi praesentium mollitia enim saepe error in dolorem fugiat beatae cupiditate nisi consequuntur nostrum ad accusamus est harum laborum, maxime nulla animi, rerum doloribus possimus? Minus adipisci, natus, labore eaque autem aliquam quidem molestiae, numquam quam id dicta. Corrupti? </p>
+            <p> 
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto alias magnam libero eveniet quibusdam! Aut, a fugit. Nostrum sit quidem animi sequi laborum? Quaerat ipsa fugit possimus quia sint consequatur quo ducimus, quos at adipisci! Voluptate enim fugiat aut aliquam impedit rerum error ea iste, expedita repellat! Esse, doloribus sit!</p>
+            <p> 
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto alias magnam libero eveniet quibusdam! Aut, a fugit. Nostrum sit quidem animi sequi laborum? Quaerat ipsa fugit possimus quia sint consequatur quo ducimus, quos at adipisci! Voluptate enim fugiat aut aliquam impedit rerum error ea iste, expedita repellat! Esse, doloribus sit!
+            </p>
+            </div>
+        </div>
+        <div id="footer">
+            Copyright &copy; 2014 Jake Wright
+
+
+</body>
+</html>
